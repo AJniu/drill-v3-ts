027592d6496358b4e69d2b11fb546073f0e983d0
AJniu <ajniu@163.com>
2023-03-01 22:10:58 +0800

    add:动态事件

--- vs-ts-xiaoman/src/views/AboutView.vue
+++ vs-ts-xiaoman/src/views/AboutView.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>新鲜功能模块</h1>
+	<div>
+      	<!-- 动态事件：与对象属性类似，使用[]包裹的元素会被解析为变量 -->
+      	<button @[clickEvent]="handleChange">单击/双击事件切换</button>
+    </div>
   </div>
+  
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+let clickEvent = ref('click')
+
+const handleChange = () => {
+  console.log(clickEvent)
+  if (clickEvent.value === 'click') {
+    clickEvent.value = 'dblclick'
+    return
+  }
+  clickEvent.value = 'click'
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
