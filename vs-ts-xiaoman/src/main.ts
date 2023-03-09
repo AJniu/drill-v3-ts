5413b3ea68776225a200d6267b2e542e9c88b2ca
unknown <aj_niu@163.com>
2023-03-09 09:31:13 +0800

    add: import element-plus

--- vs-ts-xiaoman/src/main.ts
+++ vs-ts-xiaoman/src/main.ts
@@ -4,11 +4,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 导入element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import './assets/base.scss'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
