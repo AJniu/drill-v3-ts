ad9f4dd7345a8ba1e20f0769abd7b98080ae64c4
unknown <aj_niu@163.com>
2023-03-09 09:25:31 +0800

    add: all slot type

--- vs-ts-xiaoman/src/views/main/ContentView.vue
+++ vs-ts-xiaoman/src/views/main/ContentView.vue
@@ -1,15 +1,58 @@
 <template>
     <div class="content-view">
-        content
+        <SlotChild>
+            <!-- 使用v-slot:name指定插槽名称，不指定为默认插槽v-slot:default -->
+            <template v-slot:head-slot>
+                <div>head-slot: form parent data</div>
+            </template>
+
+            <!-- v-slot可不写 -->
+            <template v-slot>
+                <div>from parent page v-slot</div>
+            </template>
+
+            <!-- 可使用#代替(v-slot:),在开发使用较多 -->
+            <!-- 对应作用域插槽第一种写法。在子组件处理数据 -->
+            <!-- <template #footer-slot="{ personName, personAge, personIndex }">
+                来自子组件作用域插槽：{{ personIndex }} --- {{ personName }} --- {{ personAge }}
+            </template> -->
+
+            <!-- 在父组件处理数据 -->
+            <template #footer-slot="{ person }">
+                <div>
+                    <div v-for="(item, index) in person" :key="item.name">
+                        来自子组件作用域插槽{{ index }} ：{{ item.name }} --- {{ item.age }}
+                    </div>
+                </div>
+
+            </template>
+
+            <!-- 动态插槽: 使用一个变量插槽名来动态渲染插槽 -->
+            <template #[dynamic]>
+                <div>from dynamic slot</div>
+            </template>
+        </SlotChild>
     </div>
 </template>
 
 <script setup lang='ts'>
+import { ref } from 'vue';
+import SlotChild from './child/SlotChild.vue';
+
+let dynamic = ref<string>('dynamic')
 
 </script>
 
 <style lang='scss' scoped>
 .content-view {
     flex: 1;
+
+    .child-comp {
+        >div {
+            height: 200px;
+            margin-top: 20px;
+            border: 1px solid saddlebrown;
+        }
+    }
 }
 </style>
---
+++ vs-ts-xiaoman/src/views/main/child/SlotChild.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="child-comp">
+        <!-- 具名插槽 -->
+        <slot name="head-slot">
+
+        </slot>
+        <slot>匿名插槽默认数据</slot>
+
+        <!-- 作用域插槽第一种写法 -->
+        <!-- <div :key="item.name" v-for="(item, index) in data">
+            <slot name="footer-slot" :personName="item.name" :personAge="item.age" :personIndex="index">
+
+            </slot>
+        </div> -->
+
+        <slot name="footer-slot" :person="data"></slot>
+
+        <slot name="dynamic"></slot>
+
+    </div>
+</template>
+
+<script setup lang='ts'>
+import { reactive } from 'vue';
+
+interface Person {
+    name: string
+    age: number
+}
+
+const data = reactive<Person[]>([
+    {
+        name: 'xx',
+        age: 23
+    },
+    {
+        name: 'xy',
+        age: 25
+    },
+    {
+        name: 'zx',
+        age: 24
+    }
+])
+</script>
+
+<style lang='scss' scoped></style>
