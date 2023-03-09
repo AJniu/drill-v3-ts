408ce914bbf4c925a639daa5445d285a32687e7d
unknown <aj_niu@163.com>
2023-03-09 10:25:50 +0800

    add:add Skeleton and AsyncComponent

---
+++ vs-ts-xiaoman/src/views/async-comp/AsyncComp.vue
@@ -0,0 +1,26 @@
+<template>
+    <Suspense>
+        <SyncView></SyncView>
+
+        <template #fallback>
+            <SkeletonViewVue></SkeletonViewVue>
+        </template>
+    </Suspense>
+</template>
+
+<script setup lang='ts'>
+import { defineAsyncComponent } from 'vue'
+import SkeletonViewVue from './SkeletonView.vue';
+
+// 模拟异步组件
+const SyncView = defineAsyncComponent(async () => {
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(1)
+        }, 2000);
+    })
+    return import("./SyncView.vue")
+})
+</script>
+
+<style lang='scss' scoped></style>
---
+++ vs-ts-xiaoman/src/views/async-comp/SyncView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="sync">
+        <div class="sync-content">
+            <div><img :src="person.data.url" alt=""></div>
+            <div class="sync-pop">
+                <div>{{ person.data.name }}</div>
+                <div>{{ person.data.age }}</div>
+            </div>
+        </div>
+        <hr>
+        <div>{{ person.data.desc }}</div>
+
+    </div>
+</template>
+
+<script setup lang='ts'>
+import axios from 'axios'
+import { reactive } from 'vue';
+interface Data {
+    data: {
+        name: string
+        age: number
+        url: string
+        desc: string
+    }
+}
+let person = reactive<Data>({
+    data: {
+        name: '',
+        age: 0,
+        url: '',
+        desc: '',
+    }
+})
+
+const { data } = await axios.get('./data.json')
+
+person.data = data
+
+</script>
+
+<style lang='scss' scoped></style>
